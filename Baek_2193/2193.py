0f95f36d2fabb553c6ad2f3c1a297e1bceb27d72
권나영 <naa0@lguplus.co.kr>
2023-09-17 10:21:37 +0900

    230917

---
+++ Baek_2193/2193.py
@@ -0,0 +1,15 @@
+def solution(n):
+    dp = [0] * (n+1)
+
+    dp[0] = 1
+    dp[1] = 1
+
+    for i in range(2, n+1):
+        dp[i] = dp[i-2] + dp[i-1]
+    
+    return dp[n-1]
+
+n = int(input())
+
+print(solution(n))
+
